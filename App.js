f0a9e9691f6fe4777357608edcb13d161feaf3ee
Alliyan <alliyan732@gmail.com>
2023-12-17 12:03:56 +0500

    signup screen added

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Text, View } from 'react-native'
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -9,28 +8,31 @@ const Stack = createNativeStackNavigator();
 // importing screens 
 import SignInHome from './src/screens/SignInHome'
 import UserSignIn from './src/screens/UserSignIn'
-// import UserSignUp from './src/screens/UserSignUp'
+import UserSignUp from './src/screens/UserSignUp'
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
         <Stack.Screen
           name="SignInHome"
           component={SignInHome}
           options={{ headerShown: false }}
         />
+
         <Stack.Screen
           name="UserSignIn"
           component={UserSignIn}
           options={{ headerShown: false }}
         />
-        {/* 
+
         <Stack.Screen
           name="UserSignUp"
           component={UserSignUp}
           options={{ headerShown: false }}
-        /> */}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   )
