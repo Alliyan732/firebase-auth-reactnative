e11700f7bc106030ecea8f379cc18e9da6c63bd1
Alliyan <alliyan732@gmail.com>
2023-12-17 13:19:48 +0500

    firebase keys removed

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -2,21 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// import {getFirestore} from 'firebase/firestore/lite'
-// import { getStorage } from "firebase/storage";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAej8XSG3m_lEZ_En9jkplWgqeH9mS6yDY",
-    authDomain: "fir-auth-80f38.firebaseapp.com",
-    projectId: "fir-auth-80f38",
-    storageBucket: "fir-auth-80f38.appspot.com",
-    messagingSenderId: "1074856892171",
-    appId: "1:1074856892171:web:cc110efab33de46ca1740e",
-    measurementId: "G-69ZFC0CMJ8"
+
+    // add your firebase config here
+    
   };
 
 // Initialize Firebase
