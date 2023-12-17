a43523949e977a4effdb33f346dde9400bce6f2d
Alliyan <alliyan732@gmail.com>
2023-12-17 12:56:01 +0500

    userEmail, logout functionality added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -44,9 +44,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   profileImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 150,
+    height: 150,
+    borderRadius: 100,
     marginBottom: 10,
   },
   welcomeText: {
